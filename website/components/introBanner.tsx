a73a89b10bc1ac7c9432f73cf883ed9cb319c1d1
MarcelLéonMrgic <-@>
2024-03-06 17:18:41 +0100

    test

---
+++ website/components/introBanner.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function IntroBanner() {
+    return (
+        <>
+            <div className="h-screen w-full bg-amber-500"></div>
+        </>
+    );
+}
