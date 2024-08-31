76b10cd82abd5acc6a4a7deced7dbc262d70b6b6
ckl <conrad.klaus@fokus.fraunhofer.de>
2024-08-31 12:47:58 +0200

    move index page to app router

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,55 @@
+import { Metadata } from 'next'
+import Alert from 'components/layout/alert'
+import Footer from 'components/layout/footer'
+import { Navbar } from "components/layout/navbar"
+import { getActivityIndicator } from "lib/api/activityIndicator"
+import "styles/index.css"
+
+export async function generateMetadata(): Promise<Metadata> {
+  const folderPath = process.env.NODE_ENV === 'production' ? '/favicon/production' : '/favicon/development'
+
+  return {
+    title: 'TuDo Makerspace',
+    description: 'Das TuDo Makerspace an der Technischen Universität Berlin kombiniert eine gemütliche Café-Atmosphäre mit einer innovativen Makerspace-Ausstattung, einschließlich Holzwerkstatt und Lötbereich, ideal für kreative Projekte und Zusammenkünfte.',
+    metadataBase: new URL(process.env.NEXT_PUBLIC_URL as string),
+    openGraph: {
+      images: '/og',
+    },
+    icons: {
+      icon: [
+        { url: `${folderPath}/favicon.ico` },
+        { url: `${folderPath}/favicon-16x16.png`, sizes: '16x16', type: 'image/png' },
+        { url: `${folderPath}/favicon-32x32.png`, sizes: '32x32', type: 'image/png' },
+      ],
+      apple: [
+        { url: `${folderPath}/apple-touch-icon.png`, sizes: '180x180' },
+      ],
+      other: [
+        { rel: 'mask-icon', url: `${folderPath}/safari-pinned-tab.svg`, color: '#000000' },
+      ],
+    },
+    manifest: '/favicon/site.webmanifest',
+    other: {
+      'msapplication-config': `${folderPath}/browserconfig.xml`,
+    },
+  }
+}
+
+export default async function RootLayout({children}: {
+  children: React.ReactNode
+}) {
+  const activityIndicator = await getActivityIndicator()
+
+  return (
+    <html lang="en">
+    <body>
+      <div className="min-h-screen">
+        <Navbar activityIndicator={activityIndicator} />
+        <Alert preview={false} />
+        <main>{children}</main>
+      </div>
+      <Footer />
+    </body>
+    </html>
+  )
+}
---
+++ frontend/app/page.tsx
@@ -0,0 +1,39 @@
+import { Suspense } from 'react';
+import Container from 'components/ui/container';
+import MoreStories from 'components/blocks/more-stories';
+import HeroPost from 'components/blocks/hero-post';
+import Banner from 'components/blocks/banner';
+import { getAllPostsForHome, fetchMediaItemsWithBackgroundSet } from 'lib/api/wordpress';
+import { convertCoverImage, convertMorePosts } from 'lib/convertApiInterfaces';
+import ClientWrapper from './ClientWrapper';
+
+export const revalidate = 1;
+
+export default async function Home() {
+  const allPosts = await getAllPostsForHome();
+  const heroPost = allPosts.welcomePage || null;
+  const morePosts = allPosts.latestPost || [];
+  const backgroundImageUrl = await fetchMediaItemsWithBackgroundSet() || null;
+
+  const coverHeroPost = heroPost ? convertCoverImage(heroPost.title!, heroPost.featuredImageUrl, heroPost.slug, "/about-us") : null;
+  const morePostsConverted = convertMorePosts(morePosts);
+
+  return (
+    <ClientWrapper>
+      <Banner backgroundImageUrl={backgroundImageUrl!} />
+      <Container>
+        {heroPost && coverHeroPost && (
+          <HeroPost
+            title={heroPost.title!}
+            coverImage={coverHeroPost}
+            slug={heroPost.slug!}
+            excerpt={heroPost.excerpt!}
+          />
+        )}
+        <Suspense fallback={<div>Loading more stories...</div>}>
+          {morePostsConverted.length > 0 && <MoreStories posts={morePostsConverted} />}
+        </Suspense>
+      </Container>
+    </ClientWrapper>
+  );
+}
