692231de4b201e46079a7271dc8cd802d4077f68
ckl <conrad.klaus@fokus.fraunhofer.de>
2025-01-31 22:36:25 +0100

    combine /events to /[category] route

--- frontend/src/app/[category]/page.tsx
+++ frontend/src/app/[category]/page.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
-import { notFound } from 'next/navigation';
-import { childPagesByParentId, getAllParentPagesAsSlug, getPageByTitle } from '@/lib/api/wordpress';
 import { Metadata } from 'next';
+import { notFound } from 'next/navigation';
+import {
+  getLatestPostsForHome,
+  getPageByTitle,
+  childPagesByParentId,
+  getAllParentPagesAsSlug
+} from '@/lib/api/wordpress';
 import Overview from '@/components/page-templates/overview';
 
-export const revalidate = 60
-
-export async function generateStaticParams() {
-  const allParentPagesSlugs = await getAllParentPagesAsSlug();
-  return allParentPagesSlugs
-    .filter(slug => slug !== 'events')
-    .map(category => ({ category }));
-}
+export const revalidate = 60;
 
 type Props = {
   params: Promise<{ category: string }>
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}
+
+export async function generateStaticParams() {
+  const allParentPagesSlugs = await getAllParentPagesAsSlug();
+  return allParentPagesSlugs.map(category => ({ category }));
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
@@ -34,13 +37,31 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 export default async function OverviewPage({ params }: Props) {
   const category = (await params).category;
   const page = await getPageByTitle(category);
-
   if (!page) {
     notFound();
   }
 
-  const childPages = await childPagesByParentId(page.id!);
+  if (category === 'events') {
+    const posts = await getLatestPostsForHome();
+    const childPages = posts.map(post => ({
+      id: null,
+      title: post.title,
+      content: post.excerpt,
+      featuredImageUrl: post.featuredImageUrl,
+      slug: post.slug,
+      parent: null
+    }));
 
+    return (
+      <Overview
+        path={'events'}
+        page={page}
+        childPages={childPages}
+      />
+    )
+  }
+
+  const childPages = await childPagesByParentId(page.id!);
   return (
     <Overview
       path={category}
